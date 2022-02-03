a7604b7597634947b061801b74a34f25b962e2af
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-03 19:06:59 +0000

    Thu, Feb 3, 2022, 7:06 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -16,7 +16,7 @@ int main(void)
     for (int i = 1; i <= n; i++)
     {
         //Print intial spaces
-        for (int j = 1; j < n - j; j++)
+        for (int j = 1; j <= n - 1; j++)
         {
             printf(".");
         }

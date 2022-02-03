d052a2ab3a81d36a7c2471c8a8705f0eea7dfd14
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-03 16:33:07 +0000

    Thu, Feb 3, 2022, 4:33 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Pyramid Size: ");
+    }
+    while (n < 1);
+
+    for (int i=0; i < n; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
 }

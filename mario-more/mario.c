44833b710223ae771d844d6a225895ac8a28001f
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-03 16:50:47 +0000

    Thu, Feb 3, 2022, 4:50 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -10,9 +10,9 @@ int main(void)
     }
     while (n < 1);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j <= n; j++)
         {
             printf("#");
         }

bc77b0584664f538761b272341bf2d59f1a10424
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-20 23:08:34 +0000

    Sun, Feb 20, 2022, 11:08 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -53,9 +53,8 @@ int main(void)
         {
             printf("MASTERCARD\n");
         }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
-    else
-    {
-        printf("INVALID\n");
-    }
-}

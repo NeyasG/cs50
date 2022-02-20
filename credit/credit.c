1a3d693b4f52701d254fb65b8a4271175e5593b8
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-20 22:02:28 +0000

    Sun, Feb 20, 2022, 10:02 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -18,20 +18,20 @@ int main(void)
         // finding individual digit
         x = ( (n/pow(10, i)));
         int y = x % 10;
-        printf("y = %i\n", y);
+        // printf("y = %i\n", y);
 
         if ( i % 2 == 0)
         {
-            sum = sum + y;
+            sum = sum + y; //adding normal digits
         }
-        else
+        else // adding the sum of the remaining digits
         {
             int z = y*2;
             int a = ( (z/pow(10, 0)));
             int b = ( (z/pow(10, 1)));
             sum = sum + a%10 + b%10;
         }
-        printf("sum = %i\n", sum);
+        // printf("sum = %i\n", sum);
 
         i ++;
     }

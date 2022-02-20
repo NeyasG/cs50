45c5fb6bf8bc8a29a6d16fc997b6dde3478d254a
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-02-20 23:04:49 +0000

    Sun, Feb 20, 2022, 11:04 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -45,11 +45,11 @@ int main(void)
         {
             printf("VISA\n");
         }
-        if (length[i-2] == 3 && (length[i-3] == 4 || length[i-3] == 7))
+        else if (length[i-2] == 3 && (length[i-3] == 4 || length[i-3] == 7))
         {
             printf("AMEX\n");
         }
-        if (length[i-2] == 5 && (length[i-3] == 1 || length[i-3] == 2 || length[i-3] == 3 || length[i-3] == 4 || length[i-3] == 5))
+        else if (length[i-2] == 5 && (length[i-3] == 1 || length[i-3] == 2 || length[i-3] == 3 || length[i-3] == 4 || length[i-3] == 5))
         {
             printf("MASTERCARD\n");
         }

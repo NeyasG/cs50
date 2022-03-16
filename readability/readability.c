c3381bdf25c1d67389cba27691e256702e3ec972
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-16 20:46:04 +0000

    Wed, Mar 16, 2022, 8:46 PM +00:00

---
+++ readability/readability.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+int count_letters(string text)
+
+{
+
+//prompt user for string input
+//count number of characters, words, and sentences
+//Exclude spaces
+//Coleman formula to identify grade
+//if <1 output "before grade 1"
+//if >16 output "Grade 16+"
+
+text = get_string("Text: ");
+printf("%s\n", text);
+
+}

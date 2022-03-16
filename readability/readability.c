11f0d7acd38484c273d1fff5c9feaaca3e08cfe3
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-16 23:34:28 +0000

    Wed, Mar 16, 2022, 11:34 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -23,27 +23,27 @@ int main(void)
     float sent = count_sentences(text);
 
 //calculate the Coleman-Liau index for text
-    float L = let/( word/100 );
-    float S = sent/( word/100 );
+    float L = let / (word / 100);
+    float S = sent / (word / 100);
     float index = (0.0588 * L) - (0.296 * S) - 15.8;
     int rounded_index = round(index);
 
 //output Grade level
-if(rounded_index < 1)
-{
-    printf("Before Grade 1\n");
-    return(0);
-}
-if(rounded_index > 16)
-{
-    printf("Grade 16+\n");
-    return(0);
-}
-else
-{
-    printf("Grade %i\n", rounded_index);
-    return(0);
-}
+    if (rounded_index < 1)
+    {
+        printf("Before Grade 1\n");
+        return (0);
+    }
+    if (rounded_index > 16)
+    {
+        printf("Grade 16+\n");
+        return (0);
+    }
+    else
+    {
+        printf("Grade %i\n", rounded_index);
+        return (0);
+    }
 
 }
 
@@ -52,13 +52,16 @@ int count_letters(string text)
 {
     int letters = 0;
     //looping through string array
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
         //checking for alphanumerics
-        if(isalnum(text[i]) != false)
-        letters ++;
-    }
-    return(letters++);
+        if (isalnum(text[i]) != false)
+
+
+            letters ++;
+        }
+
+    return (letters++);
 
 }
 
@@ -66,23 +69,29 @@ int count_letters(string text)
 int count_words(string text)
 {
     int words = 0;
-    for(int j = 0; j < strlen(text); j++)
+    for (int j = 0; j < strlen(text); j++)
     {
-        if(isspace(text[j]) != 0)
-        words++;
+        if (isspace(text[j]) != 0)
+
+
+            words++;
+
     }
+
     words = words + 1;
-    return(words);
+    return (words);
 }
 
 //function to count sentences
 int count_sentences(string text)
 {
     int sentences = 0;
-    for(int k = 0; k < strlen(text); k++)
+    for (int k = 0; k < strlen(text); k++)
     {
-        if(text[k] == '.' || text[k] == '!' || text[k] == '?')
-        sentences++;
-    }
+        if (text[k] == '.' || text[k] == '!' || text[k] == '?')
+
+
+            sentences++;
+        }
     return(sentences);
 }

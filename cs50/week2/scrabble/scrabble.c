5b1388c616342444d4c6e03aa58cdd3600797a14
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-17 13:05:00 +0000

    Thu, Mar 17, 2022, 1:05 PM +00:00

--- cs50/week2/scrabble/scrabble.c
+++ cs50/week2/scrabble/scrabble.c
@@ -20,22 +20,33 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    int word_score = 0;
-    for (int i = 0; i < strlen(word); i++)
+    int score = 0;
+    for (int i = 0, len = strlen(word); i < len; i++)
     {
         if (isupper(word[i]))
         {
-            word_score += POINTS[word[i]- "A"];
+            score += POINTS[word[i]- 'A'];
         }
         else if (islower(word[i]))
         {
-            score += POINTS[word[i] - "a"];
+            score += POINTS[word[i] - 'a'];
         }
     }
     return score;

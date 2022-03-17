d621c899a493c5f9fc1ee240125d19a44efc1e07
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-17 19:29:22 +0000

    Thu, Mar 17, 2022, 7:29 PM +00:00

--- cs50/week2/caesar/caesar.c
+++ cs50/week2/caesar/caesar.c
@@ -8,7 +8,6 @@ char rotate(char a, int x);
 bool only_digits(string digits);
 int main(int argc, string argv[])
 {
-    // printf("argv[1] = %s\n", argv[1]);
     // Get key value from command line argument
     if (argc != 2)
     {
@@ -17,8 +16,7 @@ int main(int argc, string argv[])
         return 1;
     }
 
-        // if not decimal digit print error message and return 1
-        // if error print error message and return 1
+    // if not decimal digit print error message and return 1
     if (only_digits(argv[1]) == false)
     {
         printf("Usage: ./caesar key\n");
@@ -29,23 +27,21 @@ int main(int argc, string argv[])
         // convert to int from string
         int key = atoi(argv[1]);
         string plain_text = get_string("plaintext: ");
-        //printf("%c \n", plain_text[1]);
+
         // run cipher function on string array per character
         for (int j = 0; j < strlen(plain_text); j++)
         {
             plain_text[j] = rotate(plain_text[j], key);
         }
-        //plain_text[1] = rotate(plain_text[1], 1);
         printf("ciphertext: %s\n", plain_text);
     }
     return 0;
-    //char ch = rotate('a', 2);
-    //printf("Rotated = %c\n", ch);
-    // print cipher
-    //printf("ciphertextL: %s\n", cipher_text);
-    // return 0
+
+
+
 }
 
+// function to check for non-digits in key
 bool only_digits(string s)
 {
     for (int i = 0, len = strlen(s); i < len; i++)
@@ -58,6 +54,7 @@ bool only_digits(string s)
     return true;
 }
 
+// function to apply cipher to string
 char rotate(char a, int x)
 {
     if (isupper(a))
@@ -66,8 +63,7 @@ char rotate(char a, int x)
         c = c + 'A';
         return c;
     }
-    else
-        if (islower(a))
+    else if (islower(a))
     {
         char c = ((a - 'a') + x) % 26;
         c = c + 'a';

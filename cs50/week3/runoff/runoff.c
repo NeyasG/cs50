aa339dc8557f55cc4b4c6056bbd2b1698e868c7a
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-19 19:16:26 +0000

    Sat, Mar 19, 2022, 7:16 PM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name) == 0)
         {
-            preferences[i][i] = preferences[candidate[i].name][rank];
+            preferences[i][i] = 
             return true;
         }
         return false;

369fa92a17e5d94e437fbc66b9b4a6afd748f18f
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-20 18:23:24 +0000

    Sun, Mar 20, 2022, 6:23 PM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -167,7 +167,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (round(candidates[i].votes)*2 >= voter_count)
+        if (round(candidates[i].votes)*2 > voter_count)
         {
             printf("%s\n", candidates[i].name);
             return true;

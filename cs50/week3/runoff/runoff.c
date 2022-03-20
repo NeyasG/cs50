028f7c693959ec6b606b09fa0126416c556575cb
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-20 17:50:43 +0000

    Sun, Mar 20, 2022, 5:50 PM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -181,7 +181,10 @@ int find_min(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (candidates[i].eliminated == false)
+        {
+            
+        }
     }
     return 0;
 }

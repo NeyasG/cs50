973eed2ca9d92dda2a4bd33cf3abc55c3f5769e0
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-20 19:04:18 +0000

    Sun, Mar 20, 2022, 7:04 PM +00:00

--- cs50/week4/volume/volume.c
+++ cs50/week4/volume/volume.c
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
 
-    for (int i )
+
     // Close files
     fclose(input);
     fclose(output);

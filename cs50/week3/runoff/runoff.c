30861ed6292607ad0266186fed2740112abbd19d
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-20 17:48:26 +0000

    Sun, Mar 20, 2022, 5:48 PM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -164,7 +164,6 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes >= voter_count / 2)
@@ -180,6 +179,10 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return 0;
 }
 

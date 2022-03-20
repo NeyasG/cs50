f4dffab2c51dfcecbd33667d721ea60aed72abcc
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-20 17:52:47 +0000

    Sun, Mar 20, 2022, 5:52 PM +00:00

--- cs50/week3/runoff/runoff.c
+++ cs50/week3/runoff/runoff.c
@@ -179,11 +179,12 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
+    int min_votes = voter_count;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false)
+        if (candidates[i].eliminated == false && candidates[i].votes < min_votes)
         {
-            
+            min_votes = candidates[i].votes;
         }
     }
     return 0;

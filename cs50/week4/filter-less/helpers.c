d3e45d0bdd1b80a2705ea169b13c1087ef6ca2ee
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-22 19:40:19 +0000

    Tue, Mar 22, 2022, 7:40 PM +00:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -3,7 +3,7 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i > height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -19,6 +19,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+        for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avg = (image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3.0;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+        }
+    }
     return;
 }
 

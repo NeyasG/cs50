b9cd3214f00003bdf36763a808be12beacf1dcbd
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-22 21:47:22 +0000

    Tue, Mar 22, 2022, 9:47 PM +00:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -80,5 +80,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
     return;
 }

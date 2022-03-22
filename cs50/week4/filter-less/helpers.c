8ee0d51d4c9ca1f0e1d824ee28e0be00359f8ae6
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-22 23:12:31 +0000

    Tue, Mar 22, 2022, 11:12 PM +00:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -68,9 +68,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width / 2; j++)
+        for (int j = 0; j < width; j++)
         {
-            image[i][width j] = tmp_pixel[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image [i][width - (j + 1)] = tmp_pixel[i][j];
         }
     }
     return;

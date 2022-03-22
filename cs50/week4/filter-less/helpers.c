ca94b818d264eea434a115eb964c3f64bebc2823
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-22 19:50:58 +0000

    Tue, Mar 22, 2022, 7:50 PM +00:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -23,12 +23,28 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            int max = 255;
             int originalRed = image[i][j].rgbtRed;
             int originalBlue = image[i][j].rgbtBlue;
             int originalGreen = image[i][j].rgbtGreen;
             image[i][j].rgbtBlue =  .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+            if (image[i][j].rgbtBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+
             image[i][j].rgbtRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+
             image[i][j].rgbtGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+
         }
     }
     return;

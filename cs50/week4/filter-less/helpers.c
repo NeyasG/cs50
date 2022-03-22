a27d00159af78b35670cf1f8e365bf3d7c042256
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-22 22:22:19 +0000

    Tue, Mar 22, 2022, 10:22 PM +00:00

--- cs50/week4/filter-less/helpers.c
+++ cs50/week4/filter-less/helpers.c
@@ -113,7 +113,25 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             // top right corner
             else if (i == 0 || j == (width - 1))
             {
-                sumRed = (copy[i][j].rgbtRed + copy[i + 1][j].rgbtRed + copy[i][j + 1].rgbtRed + copy[i+1][j + 1].rgbtRed)
+                sumRed = (copy[i][j].rgbtRed + copy[i + 1][j].rgbtRed + copy[i][j - 1].rgbtRed + copy[i + 1][j - 1].rgbtRed)
+                sumBlue = (copy[i][j].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i+1][j + 1].rgbtBlue)
+                sumGreen = (copy[i][j].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i][j + 1].rgbtGreen + copy[i+1][j + 1].rgbtGreen)
+                counter = 4
+            }
+
+            // bottom left corner
+            else if (i == (height - 1) || j == 0)
+            {
+                sumRed = (copy[i][j].rgbtRed + copy[i - 1][j].rgbtRed + copy[i][j + 1].rgbtRed + copy[i - 1][j + 1].rgbtRed)
+                sumBlue = (copy[i][j].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i+1][j + 1].rgbtBlue)
+                sumGreen = (copy[i][j].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i][j + 1].rgbtGreen + copy[i+1][j + 1].rgbtGreen)
+                counter = 4
+            }
+
+            // bottom right corner
+            else if (i == (height - 1) || j == (width - 1))
+            {
+                sumRed = (copy[i][j].rgbtRed + copy[i - 1][j].rgbtRed + copy[i][j + 1].rgbtRed + copy[i - 1][j + 1].rgbtRed)
                 sumBlue = (copy[i][j].rgbtBlue + copy[i + 1][j].rgbtBlue + copy[i][j + 1].rgbtBlue + copy[i+1][j + 1].rgbtBlue)
                 sumGreen = (copy[i][j].rgbtGreen + copy[i + 1][j].rgbtGreen + copy[i][j + 1].rgbtGreen + copy[i+1][j + 1].rgbtGreen)
                 counter = 4

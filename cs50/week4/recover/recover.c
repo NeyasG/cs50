9c7e361eb48ae5dfdfc731e8841cb75a242cfdbc
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-24 19:26:53 +0000

    Thu, Mar 24, 2022, 7:26 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -4,4 +4,7 @@
 int main(int argc, char *argv[])
 {
     // check that argc == 2
+        // else printf "correct usage" and return 1
+    // check if file can be opened
+        // else error message and return 1
 }

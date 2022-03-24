33d15e83ce098f3f9b3846045679ad0055af6df8
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-24 19:41:03 +0000

    Thu, Mar 24, 2022, 7:41 PM +00:00

--- cs50/week4/recover/recover.c
+++ cs50/week4/recover/recover.c
@@ -4,12 +4,15 @@
 int main(int argc, char *argv[])
 {
     // check that argc == 2
-        // else printf "correct usage" and return 1
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
     // check if file can be opened
         // else error message and return 1
     // fopen file
     // while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
         // if jpeg signature found
             // fwrite to out file with name sprintf(###.jpg [8 bytes])
-
 }

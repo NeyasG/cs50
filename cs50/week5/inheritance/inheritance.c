3b11269f7aa801c75e26660842259accd3c62501
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-29 16:56:27 +0000

    Tue, Mar 29, 2022, 4:56 PM +00:00

--- cs50/week5/inheritance/inheritance.c
+++ cs50/week5/inheritance/inheritance.c
@@ -80,7 +80,7 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (child->parents[0])
     // TODO: Free parents recursively
 
     // TODO: Free child

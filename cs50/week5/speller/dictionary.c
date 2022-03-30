6a421dccab71884599ad65e5febe040c08ec7998
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 11:01:30 +0000

    Wed, Mar 30, 2022, 11:01 AM +00:00

--- cs50/week5/speller/dictionary.c
+++ cs50/week5/speller/dictionary.c
@@ -88,7 +88,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false

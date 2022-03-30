eb4811120ffc3bef4ec0af486be893484dbca386
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 17:54:52 +0000

    Wed, Mar 30, 2022, 5:54 PM +00:00

--- cs50/week6/sentimental-mario-less/mario.py
+++ cs50/week6/sentimental-mario-less/mario.py
@@ -1,9 +1,10 @@
 import cs50
 
 while True:
-    height = input("Height ")
-    if height.isnumeric():
-        for i in range(1, height, 1):
+    height = input("Height: ")
+    if (height.isnumeric() > 0):
+        height = int(height)
+        for i in range(1, (height + 1), 1):
             print(" " * (height - i), "#" * (i), sep='')
         break
 

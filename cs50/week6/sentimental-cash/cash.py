4d7932dd5a09ac2cd3a75cf62b9ef78011a8cb1d
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 18:31:16 +0000

    Wed, Mar 30, 2022, 6:31 PM +00:00

--- cs50/week6/sentimental-cash/cash.py
+++ cs50/week6/sentimental-cash/cash.py
@@ -4,17 +4,17 @@ dollars = cs50.get_float("How many dollars: ")
 remainder = dollars;
 
 quarters = remainder / 0.25
-remainder = remainder - quarters
+remainder = remainder - (quarters * 0.25)
 
 print(remainder, quarters)
 
 dimes = remainder / 0.10
-remainder = remainder - dimes
+remainder = remainder - (dimes * 0.10)
 
 nickels = remainder / 0.05
-remainder = remainder - nickels
+remainder = remainder - (nickels * 0.05)
 
 cents = remainder / 0.01
-remainder = remainder - cents
+remainder = remainder - (cents * 0.01)
 
 print(quarters + dimes + nickels + cents)

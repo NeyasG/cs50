a4cb36e8ea36ff463daa0f7a882932900c457367
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 18:37:15 +0000

    Wed, Mar 30, 2022, 6:37 PM +00:00

--- cs50/week6/sentimental-cash/cash.py
+++ cs50/week6/sentimental-cash/cash.py
@@ -1,8 +1,17 @@
 import cs50
 
 cents = cs50.get_float("Change owed: ")
+cents = cents * 100
 
 quarters = int(cents / 25)
 cents = cents - quarters * 25
 
-nickels = int(cents / )
+dimes = int(cents / 10)
+cents = cents - dimes * 10
+
+nickels = int(cents / 5)
+cents = cents - nickels * 5
+
+pennies = int(cents / 1)
+
+print(int(quarters + dimes + nickels + cents))

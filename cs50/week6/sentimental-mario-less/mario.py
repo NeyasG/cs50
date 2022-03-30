d0606b5e4c57c357cc257083777437ccbb66a042
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 17:40:05 +0000

    Wed, Mar 30, 2022, 5:40 PM +00:00

--- cs50/week6/sentimental-mario-less/mario.py
+++ cs50/week6/sentimental-mario-less/mario.py
@@ -1,6 +1,6 @@
-height = 0
+height = ("wrong")
 
-while (height < 1):
+while (isinstance(height)):
     height = int(input("Height: "))
     for i in range(1, height, 1):
         print(" " * (height - i), "#" * (i), sep='')

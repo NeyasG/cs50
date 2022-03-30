8a264827058aca0fc6c4d02207391ecf2b16817e
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-03-30 18:08:40 +0000

    Wed, Mar 30, 2022, 6:08 PM +00:00

--- cs50/week6/sentimental-mario-more/mario.py
+++ cs50/week6/sentimental-mario-more/mario.py
@@ -4,6 +4,6 @@ while True:
     height = input("Height: ")
     if (height.isnumeric() == True and int(height) < 9 and int(height) > 0):
         for i in range(1, (int(height) + 1), 1):
-            print(" " * (int(height) - i), "#" * (i), sep='', end=' ')
+            print(" " * (int(height) - i), "#" * (i), sep='', end='  ')
             print("#" * (i), sep='',)
         break

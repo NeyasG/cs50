e79b74f9c02d0e4d60fa134db2ecdb39d65f9cfa
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-05 20:25:01 +0000

    Tue, Apr 5, 2022, 8:25 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -12,22 +12,23 @@ if re.match(regex, cc_num) == None:
 # reverse cc_num
 cc_num = cc_num[::-1]
 
-# apply algorithm to user input
+# find every second number * 2
 digits = [int(x) for x in str(cc_num)]
-# print(digits)
 y = []
-# for x in range(1, len(cc_num), 2):
-#     y = y + [digits[x] * 2]
-
 for x in range(1, len(cc_num), 2):
     y = y + [digits[x] * 2]
 
+# find sum of digits from above
 result = 0
 for elem in y:
     sum_val = 0
     for digit in str(elem):
         sum_val += int(digit)
     result += sum_val
+
+# add above to sum of remaining digits in cc_num
+for x in range(0, len(cc_num), 2):
+    result += sum(digits[x])
 
 print(result)
 # output whether valid or not

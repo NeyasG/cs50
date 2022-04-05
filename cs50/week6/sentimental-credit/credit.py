95812f8a3400628aff3521d004bf6de4de8b93a6
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-05 16:07:51 +0000

    Tue, Apr 5, 2022, 4:07 PM +00:00

---
+++ cs50/week6/sentimental-credit/credit.py
@@ -0,0 +1,15 @@
+import re
+
+# get user input
+cc_num = input("Number: ")
+regex = r"^(\d{13}|\d{15,16})$"
+
+# Validate User input
+if re.match(regex, cc_num) == None:
+    print("INVALID")
+    exit()
+
+# print("VALID") # Debug
+
+# apply algorithm to user input
+# output whether valid or not

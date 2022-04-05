f68a69ddaa52d5c846108de8c912d92ca93bc006
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-05 19:47:16 +0000

    Tue, Apr 5, 2022, 7:47 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -9,10 +9,12 @@ if re.match(regex, cc_num) == None:
     print("INVALID")
     exit()
 
+# reverse cc_num
+
 # apply algorithm to user input
 digits = [int(x) for x in str(cc_num)]
 
-for x in range(16, 2):
+for x in range(1, len(cc_num), 2):
     print(cc_num[x])
 
 # output whether valid or not

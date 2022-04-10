5d5be7c79181ef08fd083fe776c250afa65e5500
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-10 16:52:33 +0000

    Sun, Apr 10, 2022, 4:52 PM +00:00

---
+++ cs50/week6/sentimental-credit/tejas_credit.py
@@ -0,0 +1,37 @@
+def luhn_validate(card_number):
+    luhn_sum = 0
+    card_length = 0
+
+    card_number_copy = card_number
+
+    while(card_number_copy > 0):
+        digit = card_number_copy % 10
+        if card_length % 2 == 1:
+            #counting from the *right*, we double every *second* digit
+            digit *= 2
+        if digit >= 10:
+            #separating and adding together digits of any number 10-20 is the same as subtracting 9
+            #e.g. 12 = 1 + 2 = 3 = 12 - 9
+            digit -= 9
+        #this adds the remaining digits as well
+        luhn_sum += digit
+        #integer division by 10 shifts all the digits down
+        card_number_copy //= 10
+        card_length += 1
+
+    if luhn_sum % 10 != 0:
+        return "INVALID"
+
+    if card_length == 13:
+        if (4e12 <= card_number < 5e12):
+            return "VISA"
+    if card_length == 15:
+        if (34e13 <= card_number < 35e13) or (37e13 <= card_number < 38e13):
+            return "AMEX"
+    if card_length == 16:
+        if (4e15 <= card_number < 5e15):
+            return "VISA"
+        elif (51e14 <= card_number < 56e14):
+            return "MASTERCARD"
+
+    return "INVALID"

1907b20f1d697cd20bd40d6278117998ba56205e
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-10 15:07:33 +0000

    Sun, Apr 10, 2022, 3:07 PM +00:00

--- cs50/week6/sentimental-credit/credit.py
+++ cs50/week6/sentimental-credit/credit.py
@@ -4,47 +4,47 @@
 import re
 
 if __name__ == "__main__":
-# get user input
-cc_num = input("Number: ")
-regex = r"^(\d{13}|\d{15,16})$"
+    # get user input
+    cc_num = input("Number: ")
+    regex = r"^(\d{13}|\d{15,16})$"
 
-# Validate User input
-if re.match(regex, cc_num) == None:
-    print("INVALID")
-    exit()
+    # Validate User input
+    if re.match(regex, cc_num) == None:
+        print("INVALID")
+        exit()
 
-# reverse cc_num
-cc_num_rev = cc_num[::-1]
+    # reverse cc_num
+    cc_num_rev = cc_num[::-1]
 
-# find every second number * 2
-digits = [int(x) for x in str(cc_num_rev)]
-y = []
-for x in range(1, len(cc_num), 2):
-    y = y + [digits[x] * 2]
+    # find every second number * 2
+    digits = [int(x) for x in str(cc_num_rev)]
+    y = []
+    for x in range(1, len(cc_num), 2):
+        y = y + [digits[x] * 2]
 
-# find sum of digits from above
-result = 0
-for elem in y:
-    sum_val = 0
-    for digit in str(elem):
-        sum_val += int(digit)
-    result += sum_val
+    # find sum of digits from above
+    result = 0
+    for elem in y:
+        sum_val = 0
+        for digit in str(elem):
+            sum_val += int(digit)
+        result += sum_val
 
-# add above to sum of remaining digits in cc_num
-for x in range(0, len(cc_num), 2):
-    result += sum([digits[x]])
+    # add above to sum of remaining digits in cc_num
+    for x in range(0, len(cc_num), 2):
+        result += sum([digits[x]])
 
-if result % 10 != 0:
-    print("INVALID")
-    exit()
+    if result % 10 != 0:
+        print("INVALID")
+        exit()
 
-if int(cc_num[:2]) == 34 or int(cc_num[:2]) == 37:
-    print("AMEX")
-elif int(cc_num[:2]) == 51 or int(cc_num[:2]) == 52 or int(cc_num[:2]) == 53 or int(cc_num[:2]) == 54 or int(cc_num[:2]) == 55:
-    print("MASTERCARD")
-elif int(cc_num[:1]) == 4:
-    print("VISA")
-else:
-    print("INVALID")
+    if int(cc_num[:2]) == 34 or int(cc_num[:2]) == 37:
+        print("AMEX")
+    elif int(cc_num[:2]) == 51 or int(cc_num[:2]) == 52 or int(cc_num[:2]) == 53 or int(cc_num[:2]) == 54 or int(cc_num[:2]) == 55:
+        print("MASTERCARD")
+    elif int(cc_num[:1]) == 4:
+        print("VISA")
+    else:
+        print("INVALID")
 
-# output whether valid or not
+    # output whether valid or not

c6b408616b701550012a97e357f30a51565aff24
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-11 20:20:00 +0000

    Mon, Apr 11, 2022, 8:20 PM +00:00

--- cs50/week6/sentimental-readability/readability.py
+++ cs50/week6/sentimental-readability/readability.py
@@ -1,8 +1,13 @@
 import cs50
 
+# get string from user
 text = cs50.get_string("Text: ")
 
+# find number of letters, discounting non alpha chars
 letters = len([c for c in text if c.isalpha()])
 
+words = 
+
+#debug
 print(letters)
 print(text.count(" "))

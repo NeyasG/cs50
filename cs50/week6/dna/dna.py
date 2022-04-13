f8a8a1dedeb4155fd2ad353b68545d84a285bb5b
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-13 13:38:45 +0000

    Wed, Apr 13, 2022, 1:38 PM +00:00

--- cs50/week6/dna/dna.py
+++ cs50/week6/dna/dna.py
@@ -13,9 +13,11 @@ def main():
     with open(sys.argv[1], "r") as csvfile:
         database = csv.DictReader(csvfile)
         # debug
-        # for row in database:
-        #     print(row['name'], row['AGATC'])
+        for row in database:
+            #print(row['name'], row['AGATC'])
+            test = [row]
         headers = database.fieldnames
+        print(test)
 
         # Read DNA sequence file into a variable
         with open(sys.argv[2], "r") as file:
@@ -39,7 +41,7 @@ def main():
             # adds to dict with key = name and value = max_len
             STR_counts[name] = str(max_len)
 
-        print(STR_counts)
+        #print(STR_counts)
 
         # TODO: Check database for matching profiles
         # if STR_counts.items() <= database.items():

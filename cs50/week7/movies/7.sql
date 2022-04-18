ac01bdb7b1a187d71f2e0bbab4eb28ad0f1e30fa
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-18 22:19:23 +0000

    Mon, Apr 18, 2022, 10:19 PM +00:00

--- cs50/week7/movies/7.sql
+++ cs50/week7/movies/7.sql
@@ -1,3 +1,5 @@
 SELECT movies.title, ratings.rating
 FROM movies
-INNER JOIN ratings ON ratings.movie_id = movies.id;
+INNER JOIN ratings ON ratings.movie_id = movies.id
+WHERE movies.year = 2010
+ORDER BY rating DESC;

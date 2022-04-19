e64b51c4e755f8b64712da2372b8dba53c38d2b5
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-19 22:42:46 +0000

    Tue, Apr 19, 2022, 10:42 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -2,4 +2,7 @@
 
 -- Theft took place on July 28th 2021 on Humphrey Street
 
-SELECT * FROM crime_scene_reports WHERE 
+SELECT * FROM crime_scene_reports WHERE street = "Humphrey Street"
+AND year = 2021
+AND month = 7
+AND day = 28;

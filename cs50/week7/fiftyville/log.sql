db8dd639278726c8b97e2aa0ca57565fdfd30fa8
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-19 22:53:51 +0000

    Tue, Apr 19, 2022, 10:53 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -5,12 +5,10 @@
 SELECT id, description FROM crime_scene_reports WHERE street = "Humphrey Street"
 AND year = 2021
 AND month = 7
-AND day = 28;
+AND day = 28
+LIMIT 1;
 
 -- Description of crime: Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
 -- Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
-SELECT name, transcript FROM interviews WHERE
-year = 2021
-AND month = 7
-AND day = 28;
+SELECT name, transcript FROM interviews WHERE id = ;

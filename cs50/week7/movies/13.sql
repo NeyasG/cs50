89002f5a21bbef21ce6582ed4190d0d3f5dc5301
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-19 22:27:16 +0000

    Tue, Apr 19, 2022, 10:27 PM +00:00

--- cs50/week7/movies/13.sql
+++ cs50/week7/movies/13.sql
@@ -2,6 +2,5 @@ SELECT name FROM people
 WHERE id IN
 (
     SELECT person_id FROM stars WHERE movie_id IN
-    (SELECT id FROM people WHERE name = "Kevin Bacon"
-    AND birth = 1958)
+    (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)
 );

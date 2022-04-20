c5fe0ba2c1def8b3a2e42bb3405507b5a94f0a56
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-20 13:12:16 +0000

    Wed, Apr 20, 2022, 1:12 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -15,3 +15,17 @@ SELECT name, transcript FROM interviews WHERE transcript LIKE "%bakery%"
 AND year = 2021
 AND month = 7
 AND day = 28;
+
+-- Notes from interviews
+-- Ruth: Within 10 minutes of theft (10:15am) car left bakery parking lot. Check Security footage from bakery
+-- Eugene: Recognised Thief. Saw thief withdrawing money from ATM on Legget Street in the morning
+-- Raymond: Phone call to thief after leaving bakery. Planning to take earliest flight out of fiftyville on 29th July. Asked caller to purchase ticket
+-- Emma: Sucpicious person came in whispering into phone for 30 minutes. Didn't buy anything.
+
+-- Querying for Security Footage from bakery on July 28th at 10:15am.
+SELECT activity, license_plate FROM bakery_security_logs WHERE
+year = 2021
+AND month = 7
+AND day = 28
+AND hour = 10
+AND minute = 15;

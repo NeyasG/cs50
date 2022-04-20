e8370aa81f58c9d788bb4d8d1f993cfe1120db83
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-20 15:54:24 +0000

    Wed, Apr 20, 2022, 3:54 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -93,4 +93,9 @@ SELECT * FROM flights
 WHERE origin_airport_id IN
 (
     SELECT id FROM airports WHERE city = "Fiftyville"
-);
+)
+AND year = 2021
+AND month = 7
+AND day = 29
+ORDER BY hour ASC
+LIMIT 1;

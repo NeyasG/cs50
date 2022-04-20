4e5da23c17a561f3a534c1e0a6376c60352a58f1
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-20 13:19:09 +0000

    Wed, Apr 20, 2022, 1:19 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -18,14 +18,24 @@ AND day = 28;
 
 -- Notes from interviews
 -- Ruth: Within 10 minutes of theft (10:15am) car left bakery parking lot. Check Security footage from bakery
--- Eugene: Recognised Thief. Saw thief withdrawing money from ATM on Legget Street in the morning
+-- Eugene: Recognised Thief. Saw thief withdrawing money from ATM on Leggett Street in the morning
 -- Raymond: Phone call to thief after leaving bakery. Planning to take earliest flight out of fiftyville on 29th July. Asked caller to purchase ticket
 -- Emma: Sucpicious person came in whispering into phone for 30 minutes. Didn't buy anything.
 
 -- Querying for Security Footage from bakery on July 28th at 10:15am.
+-- License plates of all cars leaving bakery within timeframe
 SELECT activity, license_plate FROM bakery_security_logs WHERE
 year = 2021
 AND month = 7
 AND day = 28
 AND hour = 10
 AND minute BETWEEN 15 AND 25;
+
+-- Querying for Leggett Street ATM transactions in the morning hour <= 12
+SELECT account_number, transaction_type, amount FROM atm_transactions WHERE
+year = 2021
+AND month = 7
+AND day = 28
+-- AND hour BETWEEN 1 AND 12
+AND atm_location = "Leggett Street"
+AND transaction_type = "withdraw";

6c515d9bffab82e950e12ebb2fc0926f228a0c6e
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-04-20 15:20:46 +0000

    Wed, Apr 20, 2022, 3:20 PM +00:00

--- cs50/week7/fiftyville/log.sql
+++ cs50/week7/fiftyville/log.sql
@@ -45,16 +45,17 @@ AND atm_location = "Leggett Street"
 AND transaction_type = "withdraw";
 
 -- Following second lead, Querying phone calls around time of Theft.
+CREATE OR REPLACE VIEW caller_names AS
 SELECT duration, people.name, caller FROM phone_calls
 INNER JOIN people ON phone_calls.caller = people.phone_number
 WHERE year = 2021
 AND month = 7
 AND day = 28
-AND duration < 60
-
-SELECT duration, people.name, receiver FROM phone_calls
-INNER JOIN people ON phone_calls.receiver = people.phone_number
-WHERE year = 2021
-AND month = 7
-AND day = 28
 AND duration < 60;
+
+-- SELECT duration, people.name, receiver FROM phone_calls
+-- INNER JOIN people ON phone_calls.receiver = people.phone_number
+-- WHERE year = 2021
+-- AND month = 7
+-- AND day = 28
+-- AND duration < 60;

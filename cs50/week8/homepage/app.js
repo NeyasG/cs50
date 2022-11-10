a3af21f5ce27234ebd8aff7797a8fe0840b55a5b
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-11-10 18:44:54 +0000

    Thu, Nov 10, 2022, 6:44 PM +00:00

--- cs50/week8/homepage/app.js
+++ cs50/week8/homepage/app.js
@@ -1,11 +1,5 @@
 // Slider functionality from https://youtu.be/dzqDU9efnnk
 
-// const container = document.querySelector('.container');
-
-// document.querySelector('.slider').addEventListener('input', (e) => {
-//     container.style.setProperty('--position', `${e.target.value}%`);
-// })
-
 const container = document.querySelector('.container-slider');
 document.querySelector('.slider').addEventListener('input', (e) => {
   container.style.setProperty('--position', `${e.target.value}%`);

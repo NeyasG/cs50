dbf73a348b3643f8d6556bde1c290adb6a91a78a
NeyasG <98959794+NeyasG@users.noreply.github.com>
2022-11-29 18:27:43 +0000

    Tue, Nov 29, 2022, 6:27 PM +00:00

--- cs50/week9/birthdays/app.py
+++ cs50/week9/birthdays/app.py
@@ -26,7 +26,6 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
@@ -37,7 +36,6 @@ def index():
 
     else:
 
-        # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
 

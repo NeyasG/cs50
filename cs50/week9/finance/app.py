771321e1cff9874cebd492f0445a5d8a317532ea
NeyasG <98959794+NeyasG@users.noreply.github.com>
2023-01-14 14:25:27 +0000

    Sat, Jan 14, 2023, 2:25 PM +00:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -50,7 +50,11 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("buy.html")
+
+    elif request.method == "POST":
+        stock = lookup(request.form.get("buy"))
 
 
 @app.route("/history")

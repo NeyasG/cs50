54a6bb97e609dfb2875599bcd8bbd03dca9ff1ec
NeyasG <98959794+NeyasG@users.noreply.github.com>
2023-01-18 19:13:09 +0000

    Wed, Jan 18, 2023, 7:13 PM +00:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -47,8 +47,8 @@ def index():
     # Retrieve list of transactions for logged in user
     user = session["user_id"]
     transactions = db.execute("SELECT * from transactions WHERE user_id = ?", user)
-    name = db.execute("SELECT username from users WHERE id = ?", user)
     cash = db.execute("SELECT cash from users WHERE id = ?", user)
+    
     # Create a table to pass to index.html with all current holdings
     stocks = db.execute("SELECT symbol, SUM(cost) as cost, SUM(shares) as shares FROM transactions WHERE user_id = ? GROUP BY symbol ORDER BY shares DESC", user)
     # Total cost of all shares at current price

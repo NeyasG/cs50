3c0dd664d10ce5bc81a7e4e493e9e9e14df8f033
NeyasG <98959794+NeyasG@users.noreply.github.com>
2023-01-18 23:08:52 +0000

    Wed, Jan 18, 2023, 11:08 PM +00:00

--- cs50/week9/finance/app.py
+++ cs50/week9/finance/app.py
@@ -53,9 +53,11 @@ def index():
     # Create a table to pass to index.html with all current holdings
     stocks = db.execute(
         "SELECT symbol, SUM(cost) as cost, SUM(shares) as shares FROM transactions WHERE user_id = ? GROUP BY symbol ORDER BY shares DESC", user)
+
     # Total cost of all shares at current price
     total_cost = db.execute(
         "SELECT SUM(cost) as cost FROM transactions WHERE user_id = ?", user)
+
     # Grand Total cash + stock value at current price
     grand_total = 0
     for stock in stocks:
@@ -63,7 +65,7 @@ def index():
         grand_total += details["price"] * stock["shares"]
 
     # render index.html
-    return render_template("index.html", transactions=transactions, cash=cash, stocks=stocks, lookup=lookup, total_cost=total_cost, grand_total=grand_total)
+    return render_template("index.html", cash=cash, stocks=stocks, lookup=lookup, total_cost=total_cost, grand_total=grand_total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
